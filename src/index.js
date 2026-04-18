1ef889381c4bda8f97dcf53bcc7519c4aec44956
frontierb1 <big16635@gmail.com>
2026-04-19 03:53:56 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 require('dotenv').config();
 const express = require('express');
-const line = require('@line/bot-sdk');
 const path = require('path');
 
 const app = express();
 
-const lineConfig = {
-  channelSecret: process.env.LINE_CHANNEL_SECRET,
-  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-};
-
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.post('/webhook', (req, res, next) => {
-  let data = '';
-  req.on('data', chunk => { data += chunk; });
-  req.on('end', () => {
-    req.rawBody = Buffer.from(data);
-    next();
-  });
-}, line.middleware(lineConfig), require('./webhook'));
+app.use('/webhook', require('./webhook'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
